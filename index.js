2c78799b5a2579923e6b60c7815312c79d856bbe
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2021-03-22 14:59:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var datos;
 var success;
 $("#btnGet").on("click",function(){
-  $.getJSON( "http://172.17.255.130:8080/api/v3/requests/1100480", function( data ) {
+  $.getJSON( "https://172.17.255.130:8080/api/v3/requests/1100480", function( data ) {
     datos=data;
     console.log("Got Data")
   });
